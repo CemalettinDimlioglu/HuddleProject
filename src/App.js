3dbbf1773e1f8d3c798ba53390cf32d9668f18c9
CMLTTN <cemaldim@gmail.com>
2023-07-19 16:39:08 +0300

    app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@
 import { Container} from "./components/styles/Container.styled";
 import Card from "./components/Card"
 import content from "./content"
+import Footer from "./components/Footer";
 
 
  function App() {
    const theme = {
      colors: {
     header:"#ebfbff",
-    body:"#fff"
+    body:"#fff",
+    footer:"#003333"
     },
    };
    return (
@@ -18,8 +20,11 @@ import content from "./content"
      <ThemeProvider theme={theme}>
        <GlobalStyles />
        <Header />
-       <Container  />
+       <Container>
+
        {content.map((item,index)=>(<Card key={index} item={item}/>))}
+       </Container>
+       <Footer/>
      </ThemeProvider>
    );
  }
